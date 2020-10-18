650315eb1555505053cf6fcc30adc728b708203c
hsynates <hsynats97@gmail.com>
2020-10-18 18:22:06 +0300

    Initial Commit

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const images = [{
-    image: 'img/hsyn.jpg'
+    image: 'img/ubuntu-notebook.jpg',
+    title: 'Ubuntu',
+    text: 'Ubuntu is a Linux distribution based on Debian mostly composed of free and open-source software.',
+    link: 'https://www.ubuntu.com'
 }, {
-    image: 'img/hsyn1.jpg'
+    image: 'img/linuxmint-notebook.jpg',
+    title: 'Linux Mint',
+    text: 'Linux Mint is a community-driven Linux distribution based on Ubuntu.',
+    link: 'https://linuxmint.com/'
 }, {
-    image: 'img/hsyn2.jpg'
+    image: 'img/manjaro.jpg',
+    title: 'Manjaro Linux',
+    text: 'Manjaro is a free and open-source Linux distribution based on the Arch Linux operating system. ',
+    link: 'https://manjaro.org/'
 }, {
-    image: 'img/hsyn3.jpg'
+    image: 'img/fedora.jpg',
+    title: 'Fedora',
+    text: 'Fedora is a Linux distribution developed by the community-supported Fedora Project.',
+    link: 'https://getfedora.org/'
 }, {
-    image: 'img/hsyn4.jpg'
+    image: 'img/elemantary.jpeg',
+    title: 'ElementaryOS',
+    text: 'elementary OS is a Linux distribution based on Ubuntu LTS.',
+    link: 'https://elementary.io/'
 }, {
-    image: 'img/hsyn5.jpg'
+    image: 'img/kali.jpg',
+    title: 'Kali Linux',
+    text: 'Kali Linux is a Debian-derived Linux distribution designed for digital forensics and penetration testing.',
+    link: 'https://www.kali.org/'
 }];
 
 let index = 0;
@@ -17,6 +35,9 @@ let index = 0;
 const sliderImage = document.querySelector('.slider-img');
 const iconControl = document.querySelectorAll('.icon-slider-control');
 const sliderPreviewControl = document.querySelector('.slider-preview-control');
+const cardTitle = document.querySelector('.card-title');
+const cardText = document.querySelector('.card-text');
+const cardLink = document.querySelector('.card-link');
 // const btnSliderPreview = document.querySelectorAll('.btn-slider-preview');
 
 mainEvenetListener();
@@ -37,6 +58,9 @@ function mainEvenetListener() {
 function slider(indis) {
     indis = index;
     sliderImage.setAttribute('src', images[indis].image);
+    cardTitle.textContent = images[indis].title;
+    cardText.textContent = images[indis].text;
+    cardLink.setAttribute('href', images[indis].link);
 }
 
 function sliderControl(e) {
@@ -61,10 +85,12 @@ function createBtnSliderPreview() {
         const btnSliderPreview = document.createElement('button');
         btnSliderPreview.setAttribute('type', 'button');
         btnSliderPreview.className = 'btn-slider-preview';
+        btnSliderPreview.id = `btn-slider-preview-${i}`;
 
         const imgBtnPreview = document.createElement('img');
         imgBtnPreview.setAttribute('src', images[i].image);
         btnSliderPreview.appendChild(imgBtnPreview);
+        console.log(btnSliderPreview);
 
         sliderPreviewControl.appendChild(btnSliderPreview);
 
